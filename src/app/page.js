b3ae141eb4e4ad4b61d497baa4323c0d847b0b1f
Ujjwal <ujjwaljindal835@gmail.com>
2024-06-04 13:39:06 +0530

    fixed home page , usermanagement page, meta data and some styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Serve Actions</h1>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-xl font-extrabold py-48 px-24">
+        This is the Home Page for User Management System
+      </h1>
+      <Link href="/user-management">
+        <Button>User Management</Button>
+      </Link>
     </div>
   );
 }
--- src/app/user-management/page.js
+++ src/app/user-management/page.js
@@ -1,6 +1,8 @@
 import { fetchUsersAction } from "@/actions";
 import AddNewUser from "@/components/add-new-user";
 import SingleUserCard from "@/components/single-user-card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 export default async function UserManagement() {
@@ -8,9 +10,14 @@ export default async function UserManagement() {
   console.log(getListOfUsers);
   return (
     <div className="p-20 max-w-6xl">
-      <div className="flex justify-between">
-        <h1>User Management</h1>
-        <AddNewUser />
+      <div className="flex justify-between gap-4">
+        <h1 className="text-xl font-extrabold">User Management</h1>
+        <div className="flex justify-center items-center gap-3">
+          <Link href="/">
+            <Button>Home</Button>
+          </Link>
+          <AddNewUser />
+        </div>
       </div>
       <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
         {getListOfUsers &&
